fce4a89206afd317ddb64af4bdf7befdcf8a4c50
Stefan Cohen <stefan.cohen@gmail.com>
2019-08-25 22:29:47 +0200

    first commit

---
+++ octoprint_dashboard/__init__.py
@@ -0,0 +1,74 @@
+# coding=utf-8
+from __future__ import absolute_import
+
+import octoprint.plugin
+
+class DashboardPlugin(octoprint.plugin.SettingsPlugin,
+                      octoprint.plugin.StartupPlugin,
+                      octoprint.plugin.AssetPlugin,
+                      octoprint.plugin.TemplatePlugin):
+
+	def on_after_startup(self):
+        	self._logger.info("Dashboard started")
+
+	##~~ SettingsPlugin mixin
+
+	def get_settings_defaults(self):
+		return dict(
+			# put your plugin's default settings here
+		)
+
+	##~~ AssetPlugin mixin
+
+	def get_assets(self):
+		# Define your plugin's asset files to automatically include in the
+		# core UI here.
+		return dict(
+			js=["js/dashboard.js"],
+			css=["css/dashboard.css"],
+			less=["less/dashboard.less"]
+		)
+
+
+
+	##~~ Softwareupdate hook
+
+	def get_update_information(self):
+		# Define the configuration for your plugin to use with the Software Update
+		# Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
+		# for details.
+		return dict(
+			dashboard=dict(
+				displayName="Dashboard Plugin",
+				displayVersion=self._plugin_version,
+
+				# version check: github repository
+				type="github_release",
+				user="StefanCohen",
+				repo="OctoPrint-Dashboard",
+				current=self._plugin_version,
+
+				# update method: pip
+				pip="https://github.com/StefanCohen/OctoPrint-Dashboard/archive/{target_version}.zip"
+			)
+		)
+
+
+__plugin_name__ = "Dashboard"
+
+def __plugin_load__():
+	global __plugin_implementation__
+	__plugin_implementation__ = DashboardPlugin()
+
+	global __plugin_hooks__
+	__plugin_hooks__ = {
+		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+	}
+	
+	global __plugin_settings_overlay__
+	__plugin_settings_overlay__ = dict(appearance=dict(components=dict(order=dict(tab=["plugin_dashboard",
+                                                                                        "temperature",
+                                                                                        "control",
+                                                                                        "gcodeviewer",
+                                                                                        "terminal",
+                                                                                        "timelapse"]))))
---
+++ octoprint_dashboard/static/js/dashboard.js
@@ -0,0 +1,49 @@
+/*
+ * View model for OctoPrint-Dashboard
+ *
+ * Author: Stefan Cohen
+ * License: AGPLv3
+ */
+$(function() {
+    function DashboardViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+	self.temperatureModel = parameters[0];
+
+	self.formatTemperature = function(toolName, actual, target) {
+            var output = toolName + ": " + _.sprintf("%.1f&deg;C", actual);
+        
+            if (target) {
+                var sign = (target >= actual) ? " \u21D7 " : " \u21D8 ";
+                output += sign + _.sprintf("%.1f&deg;C", target);
+            }
+        
+            return output;
+        };
+
+
+
+    }
+
+
+
+
+
+
+
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: DashboardViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ... "connectionViewModel", "printerProfilesViewModel", "printerStateViewModel"
+        dependencies: [  "temperatureViewModel", "printerStateViewModel" ],
+        // Elements to bind to, e.g. #settings_plugin_dashboard, #tab_plugin_dashboard, ...
+        elements: [ "#tab_plugin_dashboard" ]
+    });
+});
