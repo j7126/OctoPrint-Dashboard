62d7c3bbeaf81512e49b038f168e34ab6e4489fd
Stefan Cohen <stefan.cohen@gmail.com>
2019-08-28 15:06:23 +0200

    Added layer, height and fanspeed widgets

--- octoprint_dashboard/__init__.py
+++ octoprint_dashboard/__init__.py
@@ -7,77 +7,76 @@ class DashboardPlugin(octoprint.plugin.SettingsPlugin,
                       octoprint.plugin.StartupPlugin,
                       octoprint.plugin.AssetPlugin,
                       octoprint.plugin.TemplatePlugin,
-		      octoprint.plugin.EventHandlerPlugin):
+                      octoprint.plugin.EventHandlerPlugin):
 
-	def __init__(self):	
-		_currentLayer = "-"
+    def on_after_startup(self):
+            self._logger.info("Dashboard started")
 
-	def on_after_startup(self):
-        	self._logger.info("Dashboard started")
+    def on_event(self, event, payload):
+        if event == "DisplayLayerProgress_layerChanged" or event == "DisplayLayerProgress_progressChanged":
+            #self._logger.info(payload.get('currentLayer'))
+            self._plugin_manager.send_plugin_message(self._identifier, dict(totalLayer=payload.get('totalLayer'), 
+                                                                            currentLayer=payload.get('currentLayer'), 
+                                                                            currentHeight=payload.get('currentHeight'), 
+                                                                            totalHeightWithExtrusion=payload.get('totalHeightWithExtrusion'), 
+                                                                            feedrate=payload.get('feedrate'), 
+                                                                            feedrateG0=payload.get('feedrateG0'), 
+                                                                            feedrateG1=payload.get('feedrateG1'), 
+                                                                            fanspeed=payload.get('fanspeed'), 
+                                                                            lastLayerDuration=payload.get('lastLayerDuration'), 
+                                                                            averageLayerDuration=payload.get('averageLayerDuration')))
 
-	def on_event(self, event, payload):
-		if event == "DisplayLayerProgress_layerChanged":
-        		self._logger.info("### Layer changed")
-			self._logger.info(payload.get('currentLayer'))
-			self._currentLayer = payload.get('currentLayer') #REMOVEME
-			self._plugin_manager.send_plugin_message(self._identifier, dict(currentLayer=payload.get('currentLayer')))
+    ##~~ SettingsPlugin mixin
+    def get_settings_defaults(self):
+        return dict(
+            # put your plugin's default settings here
+        )
 
-	##~~ SettingsPlugin mixin
+    ##~~ AssetPlugin mixin
+    def get_assets(self):
+        # Define your plugin's asset files to automatically include in the
+        # core UI here.
+        return dict(
+            js=["js/dashboard.js"],
+            css=["css/dashboard.css"],
+            less=["less/dashboard.less"]
+        )
 
-	def get_settings_defaults(self):
-		return dict(
-			# put your plugin's default settings here
-		)
+    ##~~ Softwareupdate hook
+    def get_update_information(self):
+        # Define the configuration for your plugin to use with the Software Update
+        # Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
+        # for details.
+        return dict(
+            dashboard=dict(
+                displayName="Dashboard Plugin",
+                displayVersion=self._plugin_version,
 
-	##~~ AssetPlugin mixin
+                # version check: github repository
+                type="github_release",
+                user="StefanCohen",
+                repo="OctoPrint-Dashboard",
+                current=self._plugin_version,
 
-	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
-		return dict(
-			js=["js/dashboard.js"],
-			css=["css/dashboard.css"],
-			less=["less/dashboard.less"]
-		)
-
-
-
-	##~~ Softwareupdate hook
-
-	def get_update_information(self):
-		# Define the configuration for your plugin to use with the Software Update
-		# Plugin here. See https://github.com/foosel/OctoPrint/wiki/Plugin:-Software-Update
-		# for details.
-		return dict(
-			dashboard=dict(
-				displayName="Dashboard Plugin",
-				displayVersion=self._plugin_version,
-
-				# version check: github repository
-				type="github_release",
-				user="StefanCohen",
-				repo="OctoPrint-Dashboard",
-				current=self._plugin_version,
-
-				# update method: pip
-				pip="https://github.com/StefanCohen/OctoPrint-Dashboard/archive/{target_version}.zip"
-			)
-		)
+                # update method: pip
+                pip="https://github.com/StefanCohen/OctoPrint-Dashboard/archive/{target_version}.zip"
+            )
+        )
 
 
 __plugin_name__ = "Dashboard"
 
 def __plugin_load__():
-	global __plugin_implementation__
-	__plugin_implementation__ = DashboardPlugin()
+    global __plugin_implementation__
+    __plugin_implementation__ = DashboardPlugin()
 
-	global __plugin_hooks__
-	__plugin_hooks__ = {
-		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
-	}
-	
-	global __plugin_settings_overlay__
-	__plugin_settings_overlay__ = dict(appearance=dict(components=dict(order=dict(tab=["plugin_dashboard",
+    global __plugin_hooks__
+    __plugin_hooks__ = {
+        "octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
+    }
+    
+    global __plugin_settings_overlay__
+    __plugin_settings_overlay__ = dict(appearance=dict(components=dict(order=dict(tab=["plugin_dashboard",
                                                                                         "temperature",
                                                                                         "control",
                                                                                         "gcodeviewer",
--- octoprint_dashboard/static/js/dashboard.js
+++ octoprint_dashboard/static/js/dashboard.js
@@ -10,46 +10,63 @@ $(function() {
 
         // self.loginStateViewModel = parameters[0];
         // self.settingsViewModel = parameters[1];
-	self.temperatureModel = parameters[0];
-	self.printerStateModel = parameters[1];
-	self.printerProfilesModel = parameters[2];
-	self.connectionModel = parameters[3];
-	self.settingsViewModel = parameters[4];
-	self.displaylayerprogressViewModel = parameters[5];
+        self.temperatureModel = parameters[0];
+        self.printerStateModel = parameters[1];
+        self.printerProfilesModel = parameters[2];
+        self.connectionModel = parameters[3];
+        self.settingsViewModel = parameters[4];
+        self.displaylayerprogressViewModel = parameters[5];
 
-	self.currentLayer = ko.observable();
+        self.totalLayer = ko.observable("-");
+        self.currentLayer = ko.observable(0);
+        self.currentHeight = ko.observable("-");
+        self.totalHeightWithExtrusion = ko.observable("-");
+        self.feedrate = ko.observable("-");
+        self.feedrateG0 = ko.observable("-");
+        self.feedrateG1 = ko.observable("-");
+        self.fanspeed = ko.observable("-");
+        self.lastLayerDuration = ko.observable("-");
+        self.fanspeed = ko.observable("-");
+        self.averageLayerDuration = ko.observable("-");
 
-	self.currentLayer = 0;
+        
+        //Notify user if displaylayerprogress plugin is not installed
+        if (!self.displaylayerprogressViewModel) {
+            //TODO: Display in UI instead of console log.
+            console.log("displaylayerprogressViewModel is not loaded. Is plugin installed?");
+        }
 
-	if (!self.displaylayerprogressViewModel) {
-		console.log("displaylayerprogressViewModel is not loaded. Is plugin installed?");
-	}
+        //Events from displaylayerprogress Plugin
+        self.onDataUpdaterPluginMessage = function(plugin, data) {
+            if (plugin != "dashboard") {
+                return;
+                }
+            if (data.totalLayer) { self.totalLayer(parseInt(data.totalLayer) + 1); }
+            if (data.currentLayer) { self.currentLayer(parseInt(data.currentLayer) + 1); }
+            if (data.currentHeight) { self.currentHeight(data.currentHeight); }
+            if (data.totalHeightWithExtrusion) { self.totalHeightWithExtrusion(data.totalHeightWithExtrusion); }
+            if (data.feedrate) { self.feedrate(data.feedrate); }
+            if (data.feedrateG0) { self.feedrateG0(data.feedrateG0); }
+            if (data.feedrateG1) { self.feedrateG1(data.feedrateG1); }
+            if (data.fanspeed) { self.fanspeed(data.fanspeed); }
+            if (data.lastLayerDuration) { self.lastLayerDuration(data.lastLayerDuration); }
+            if (data.averageLayerDuration) { self.averageLayerDuration(data.averageLayerDuration); }
+        };
 
 
-	self.onDataUpdaterPluginMessage = function(plugin, data) {
-		if (plugin != "dashboard") {
-			return;
-			}
-		if (data.currentLayer) {
-			console.log(data.currentLayer);
-           		self.currentLayer = data.currentLayer;
-		}
-	};
-
-
-	 self.formatConnectionstatus = function(currentStatus) {
-            if (currentStatus) {
-		return "Connected";
+        self.formatConnectionstatus = function(currentStatus) {
+                if (currentStatus) {
+                return "Connected";
             }
             else return "Disconnected";
-        };
-    }
+            };
+        }
 
-    /* view model class, parameters for constructor, container to bind to  */
+    // view model class, parameters for constructor, container to bind to
     OCTOPRINT_VIEWMODELS.push({
         construct: DashboardViewModel,
         dependencies: [  "temperatureViewModel", "printerStateViewModel", "printerProfilesViewModel", "connectionViewModel", "settingsViewModel", "displaylayerprogressViewModel" ],
-	optional: [ "displaylayerprogressViewModel" ],
+        optional: [ "displaylayerprogressViewModel" ],
         elements: [ "#tab_plugin_dashboard" ]
     });
 });
