9b5bff518ab1ff133a5d126574e1457ea9cdd8e3
Jefferey Neuffer <jeffereyneuffer@gmail.com>
2021-08-01 20:31:08 +1000

    Add licence notice to files

--- octoprint_dashboard/static/js/dashboard.js
+++ octoprint_dashboard/static/js/dashboard.js
@@ -1,9 +1,23 @@
 /*
  * View model for OctoPrint-Dashboard
  *
- * Authors: Stefan Cohen, j7126, Willmac16, CynanX
- * License: AGPLv3
- */
+ * Authors: Jefferey Neuffer (https://github.com/j7126), Will MacCormack (https://github.com/willmac16), Stefan Cohen (https://github.com/StefanCohen), CynanX
+ * 
+ * Copyright (C) 2021
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 $(function () {
     function DashboardViewModel(parameters) {
         var self = this;
