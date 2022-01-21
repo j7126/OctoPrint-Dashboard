5b27cb168a5e74cefb1abdc4737fd812f03fb104
gpread <59426970+gpread@users.noreply.github.com>
2022-01-21 07:17:26 +0000

    RockChip CPU temp reporting #327
    Added a case to psUtilGetStats() for CPU temperature monitoring on the RockPi platform

--- octoprint_dashboard/__init__.py
+++ octoprint_dashboard/__init__.py
@@ -176,6 +176,8 @@ class DashboardPlugin(octoprint.plugin.SettingsPlugin,
                 cpu_val = tempFile.read()
                 tempFile.close()
                 self.cpu_temp = int(round(float(cpu_val)/1000))
+            elif "cpu" in thermal: # RockPi (probably all RockChip CPUs/SOCs)
+                self.cpu_temp = int(round((thermal["cpu"][0][1])))				
             self.cpu_percent = str(psutil.cpu_percent(interval=None, percpu=False))
             self.cpu_freq = str(int(round(psutil.cpu_freq(percpu=False).current, 0)))
             self.virtual_memory_percent = str(psutil.virtual_memory().percent)
