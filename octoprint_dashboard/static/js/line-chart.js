68b9e2435bb59575c576c598b03891f74d93f480
Jefferey Neuffer <jeffereyneuffer@gmail.com>
2022-02-27 00:09:36 +1000

    organise js files and update vue-grid-layout

---
+++ octoprint_dashboard/static/js/line-chart.js
@@ -0,0 +1,77 @@
+Vue.component('line-chart', {
+    extends: VueChartJs.Line,
+    data: function () {
+        return {
+            data: [0],
+            labels: [''],
+            active: true
+        };
+    },
+    props: ['value'],
+    watch: {
+        value: function (val) {
+            var self = this;
+            var d = function () {
+                self.data.push(val);
+                if (self.data.length > 50) {
+                    self.data.splice(0, 1);
+                } else
+                    self.labels.push('');
+                if (self.active)
+                    self.$data._chart.update();
+            };
+            d();
+            var interval = null;
+            interval = setInterval(() => {
+                setTimeout(() => {
+                    if (self.data[self.data.length - 1] == val) {
+                        d();
+                    } else {
+                        clearInterval(interval);
+                    }
+                }, 100);
+            }, 3000);
+        }
+    },
+    mounted() {
+        this.renderChart({
+            labels: this.labels,
+            datasets: [
+                {
+                    label: '',
+                    borderColor: '#01878670',
+                    backgroundColor: '#cbe7e670',
+                    pointBackgroundColor: '#ffffff00',
+                    pointBorderColor: '#ffffff00',
+                    data: this.data
+                }
+            ]
+        }, {
+            responsive: true,
+            maintainAspectRatio: false,
+            legend: {
+                display: false
+            },
+            scales: {
+                yAxes: [{
+                    display: false,
+                    gridLines: {
+                        display: false
+                    }
+                }],
+                xAxes: [{
+                    display: false,
+                    gridLines: {
+                        display: false
+                    }
+                }]
+            }
+        });
+    },
+    activated() {
+        this.active = true;
+    },
+    deactivated() {
+        this.active = false;
+    }
+});
